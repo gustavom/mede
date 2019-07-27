0194d1d8e68bad7f96aebd06dafc83e5c78d316e
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2019-07-27 01:36:06 -0300

    add extra font weight

--- src/assets/styles/global.js
+++ src/assets/styles/global.js
@@ -1,7 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700,900&display=swap');
+  @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900&display=swap');
   *{
     margin: 0;
     padding: 0;
