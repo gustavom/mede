2682445438f56d77d6ce8af50e0771e9756b6184
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2019-07-27 00:47:23 -0300

    route test

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
-
+import {Link} from 'react-router-dom';
 // import { Container } from './styles';
 
 export default function Home() {
   return (
+    <>
+    <Link to="/panel">
     <h1>Home</h1>
+    </Link>
+    </>
   );
 }
