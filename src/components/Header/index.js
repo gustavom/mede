28e73efe5dafdbb3200e665df3974328f6bde3a6
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2019-07-27 01:10:09 -0300

    header init

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Header() {
   return (
-    <h1>Header</h1>
+    <Container>
+      <h1>Header</h1>
+    </Container>
   );
 }
