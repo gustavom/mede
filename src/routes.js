ffabf96efb47d0ea366b147c0e2116051e5dee74
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2019-07-26 23:16:18 -0300

    create routes file

---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import {Switch, Route} from 'react-router-dom';
+
+import Home from './pages/Home';
+import Panel from './pages/Panel';
+
+export default function Routes(){
+  return (
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/panel" component={Panel} />
+    </Switch>
+  )
+}
