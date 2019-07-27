0ef8c7ace665052d889bf3df59cb7ac51601c115
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2019-07-27 01:00:51 -0300

    init home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
-// import { Container } from './styles';
+import { Link } from 'react-router-dom';
+import { Container } from './styles';
 
 export default function Home() {
   return (
-    <>
-    <Link to="/panel">
-    <h1>Home</h1>
-    </Link>
-    </>
+    <Container>
+      <h1>Home</h1>
+    </Container>
   );
 }
