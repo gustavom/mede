a8f02d5e6579347930aef463ad8e4a19b3e448e9
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2019-07-28 22:36:57 -0300

    style login

--- src/components/Login/styles.js
+++ src/components/Login/styles.js
@@ -1,1 +1,11 @@
 import styled from 'styled-components';
+
+export const Container = styled.span`
+  font-size: 14px;
+  font-weight: 600;
+  cursor: pointer;
+  color: #222222;
+  &:hover {
+    color: #666;
+  }
+`;
