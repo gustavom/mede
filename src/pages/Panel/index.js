043046754959efe695d5493dbfd74c1092c68b50
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2019-07-29 19:08:37 -0300

    add speed component

--- src/pages/Panel/index.js
+++ src/pages/Panel/index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 
 import { Container } from './styles';
 
+import Speed from '../../components/Speed';
+
 export default class Panel extends Component {
   componentDidMount() {
     return this.props.navIsHome(false);
@@ -9,7 +11,7 @@ export default class Panel extends Component {
   render() {
     return (
       <Container>
-        <h1>Panel</h1>
+        <Speed />
       </Container>
     );
   }
