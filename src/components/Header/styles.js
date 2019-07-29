7421534f88eade1188a3eae6b825c7551ef0bb91
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2019-07-29 18:38:03 -0300

    header style internal page

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,18 +1,22 @@
 import styled from 'styled-components';
 
 export const Container = styled.header`
+  position: absolute;
+  z-index: 10;
   display: flex;
   align-items: center;
   justify-content: space-between;
   width: 100%;
   min-height: 90px;
   padding: 15px;
-  border-bottom: solid 1px #eee;
+  background-color: rgba(255, 255, 255, 0.05);
+  border-bottom: solid 1px rgba(238, 238, 238, 0.1);
   .logo {
     width: 100px;
     height: 25px;
     path {
-      fill: #222;
+      /* fill: #222; */
+      fill: #fff;
     }
   }
 `;
