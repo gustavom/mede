d1ea8ebcf943fd027f5eeb4e3e1c09e9f2567552
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2019-07-28 22:36:36 -0300

    import login

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 import { Container } from './styles';
-
+import Login from '../../components/Login';
 import Logo from '../../assets/img/MEDE.svg';
 
 export default function Header() {
   return (
     <Container>
       <Logo />
+      <Login />
     </Container>
   );
 }
