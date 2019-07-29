5adc21caa7c7449d052aa2faccebf906a9f0cf3b
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2019-07-28 22:36:52 -0300

    add container

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default class Login extends Component {
   render() {
     return (
-      <>
+      <Container>
         <span>login</span>
-      </>
+      </Container>
     );
   }
 }
