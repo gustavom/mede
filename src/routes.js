0b57a944d5b89c5b63ed5aa81b8c691cfabea371
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2019-07-28 21:40:05 -0300

    passing props

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Panel from './pages/Panel';
 
-export default function Routes(){
+export default function Routes({ isHome, confirmIndex }) {
   return (
     <Switch>
-      <Route path="/" exact component={Home} />
-      <Route path="/panel" component={Panel} />
+      <Route path="/" exact render={() => <Home navIsHome={confirmIndex} />} />
+      <Route path="/panel" render={() => <Panel navIsHome={isHome} />} />
     </Switch>
-  )
+  );
 }
