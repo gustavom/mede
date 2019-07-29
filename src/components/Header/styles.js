f4afbe8d5b3021b2a71f0ac87529dbab001ed681
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2019-07-28 22:10:09 -0300

    style header

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,3 +1,14 @@
 import styled from 'styled-components';
 
-export const Container = styled.header``;
+export const Container = styled.header`
+  width: 100%;
+  padding: 30px 15px;
+  border-bottom: solid 1px #eee;
+  svg {
+    width: 100px;
+    height: 25px;
+    path {
+      fill: #222;
+    }
+  }
+`;
