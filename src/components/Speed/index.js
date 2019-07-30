e17a973f6ab25cbe312849fcd52e210b3a2602a4
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2019-07-29 22:06:17 -0300

    counter speed

--- src/components/Speed/index.js
+++ src/components/Speed/index.js
@@ -8,15 +8,15 @@ export default class Speed extends Component {
       <Container>
         <div className="speed-main">
           <span className="title">Sua velocidade atual:</span>
-          <span className="numbers">150</span> Mbps
+          <span className="numbers">{this.props.download}</span> Mbps
         </div>
         <div className="speed-item">
           <span className="title">Upload</span>
-          <span className="numbers">150</span> Mbps
+          <span className="numbers">{this.props.upload}</span> Mbps
         </div>
         <div className="speed-item">
           <span className="title">Latência</span>
-          <span className="numbers">150</span> Mbps
+          <span className="numbers">{this.props.latency}</span> Mbps
         </div>
       </Container>
     );
--- src/pages/Panel/index.js
+++ src/pages/Panel/index.js
@@ -5,13 +5,40 @@ import { Container } from './styles';
 import Speed from '../../components/Speed';
 
 export default class Panel extends Component {
+  state = {
+    download: 0,
+    upload: 0,
+    latency: 0,
+  };
   componentDidMount() {
+    this.runDownload();
     return this.props.navIsHome(false);
   }
+  runDownload = () => {
+    // alert('teste');
+    let counter = 0;
+    const myTimer = () => {
+      var timer = setTimeout(() => {
+        console.log(counter++);
+        if (counter < 100) {
+          this.setState({
+            download: counter++,
+          });
+          myTimer();
+        }
+      }, 100);
+    };
+
+    myTimer();
+  };
   render() {
     return (
       <Container>
-        <Speed />
+        <Speed
+          download={this.state.download}
+          upload={this.state.upload}
+          latency={this.state.latency}
+        />
       </Container>
     );
   }
